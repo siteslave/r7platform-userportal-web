ddb7fd12daf4ebc43293e2f690311f4306cb00e4
Satit Rianpit <siteslave@users.noreply.github.com>
2023-01-23 16:24:03 +0700

    Add user service

--- src/app/features/users/services/user.service.ts
+++ src/app/features/users/services/user.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor () { }
+  private axiosInstance = axios.create({
+    baseURL: environment.apiUrl
+  });
+
+  constructor () {
+    this.axiosInstance.interceptors.request.use(config => {
+      const token = sessionStorage.getItem('token');
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+      return config;
+    });
+
+    this.axiosInstance.interceptors.response.use(response => {
+      return response;
+    }, error => {
+      return Promise.reject(error);
+    })
+  }
+
+  async getUserList(provinceCode: any = '') {
+    const url = `/users?province_code=${provinceCode}`;
+    return this.axiosInstance.get(url);
+  }
 }
--- src/app/features/users/users.component.ts
+++ src/app/features/users/users.component.ts
@@ -1,7 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { AxiosError } from 'axios';
+import { NzMessageService } from 'ng-zorro-antd/message';
+
 import { UserList } from '../../core/model/user';
 import { ModalNewUserComponent } from './modals/modal-new-user/modal-new-user.component';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-users',
@@ -10,7 +14,14 @@ import { ModalNewUserComponent } from './modals/modal-new-user/modal-new-user.co
 })
 export class UsersComponent {
 
-  constructor (private router: Router) { }
+  constructor (
+    private router: Router,
+    private userService: UserService,
+    private message: NzMessageService) { }
+
+  ngOnInit() {
+    this.getUserList();
+  }
 
   @ViewChild('mdlNewUser') private mdlNewUser: ModalNewUserComponent = new ModalNewUserComponent();
 
@@ -48,4 +59,16 @@ export class UsersComponent {
   }
 
   onSubmitRegister(event: any) { }
+
+  async getUserList() {
+    const messageId = this.message.loading('Loading...', { nzDuration: 0 }).messageId;
+    try {
+      const data = await this.userService.getUserList();
+      console.log(data);
+      this.message.remove(messageId);
+    } catch (error: any) {
+      this.message.remove(messageId);
+      this.message.error(`${error.code} - ${error.message}`);
+    }
+  }
 }
