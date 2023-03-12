8fd1c49f85f46fc84ca9a0617382b121e5995fad
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-12 09:07:49 +0700

    fixed: Default route after login

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -51,7 +51,7 @@ export class LoginComponent {
           const token = response.data.access_token;
           sessionStorage.setItem('token', token);
           this.message.create('success', 'เข้าสู่ระบบสำเร็จ');
-          this.router.navigate(['/drugs']);
+          this.router.navigate(['/dashboard']);
 
           return;
         } else {
